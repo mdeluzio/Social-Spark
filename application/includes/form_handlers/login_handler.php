ae4c2368755ae49fb957874eb8402ca7ce539fd9
Mark DeLuzio <deluziomark@gmail.com>
2020-05-05 20:00:53 -0400

    added code to handle reopening closed user account on login

--- application/includes/form_handlers/login_handler.php
+++ application/includes/form_handlers/login_handler.php
@@ -15,6 +15,11 @@
             $row = mysqli_fetch_array($check_database_query);
             $username = $row['username'];
             
+            $user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+            if(mysqli_num_rows($user_closed_query) == 1) {
+                $reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
+            }
+            
             $_SESSION['username'] = $username;
             header("Location: ../index.php");
             exit();

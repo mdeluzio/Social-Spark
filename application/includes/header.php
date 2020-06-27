5e3c0d30975a6cb566143b95b0384c14486bc2f8
Mark DeLuzio <deluziomark@gmail.com>
2020-06-27 13:24:55 -0400

    added links to nav bar in header.php

--- application/includes/header.php
+++ application/includes/header.php
@@ -3,6 +3,8 @@
     
     if(isset($_SESSION['username'])) {
        $userLoggedIn =  $_SESSION['username'];
+       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+       $user = mysqli_fetch_array($user_details_query);
     } else {
         //temp code to control sessions before implementing logout functionality
         header("Location: application/register.php");
@@ -20,6 +22,7 @@
     <script src="application/assets/js/bootstrap.js"></script>
     
     <!-- CSS -->
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>
     <link rel="stylesheet" type="text/css" href="application/assets/css/bootstrap.css"></link>
     <link rel="stylesheet" type="text/css" href="application/assets/css/style.css"></link>
 
@@ -30,4 +33,25 @@
         <div class="logo">
             <a href="index.php">Social Spark!</a>
         </div>
+    
+        <nav>
+            <a class="my_profile_link" href="#">
+                <?php echo $user["first_name"]; ?>
+            </a>
+            <a class="home_link" href="index.php">
+                <i class="fa fa-home fa-lg"></i>
+            </a>
+            <a class="messages_link" href="#">
+                <i class="fa fa-envelope fa-lg"></i>
+            </a>
+            <a class="notification_link" href="#">
+                <i class="fa fa-bell fa-lg"></i>
+            </a>
+            <a class="friends_link" href="#">
+                <i class="fa fa-users fa-lg"></i>
+            </a>
+            <a class="settings_link" href="#">
+                <i class="fa fa-cog fa-lg"></i>
+            </a>
+        </nav>
     </div>

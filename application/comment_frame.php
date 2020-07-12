3fe20a46414d73cbb176eef71a716c87bbe260a3
Mark DeLuzio <deluziomark@gmail.com>
2020-07-12 08:31:38 -0400

    working on loading comments. Currently dealing with a bug

--- application/comment_frame.php
+++ application/comment_frame.php
@@ -63,8 +63,91 @@
     </form>
     
     <!-- Load Comments -->
+    <?php
+        $get_comments= mysqli_query($con, "SELECT * FROM comments WHERE post_id='$post_id' ORDER BY id ASC");
+        $count = mysqli_num_rows($get_comments);
+        if($count != 0) {
+            
+            while($comment = mysqli_fetch_array($get_comments)) {
+                $comment_body = $comment['post_body'];
+                $posted_to = $comment['posted_to'];
+                $posted_by = $comment['posted_by'];
+                $date_added = $comment['date_added'];
+                $removed = $comment['removed'];
+                
+                //Timeframe
+                $date_time_now = date("Y-m-d H:i:s");
+                $start_date = new DateTime($date_added); // Time of post
+                $end_date = new DateTime($date_time_now); // Current Time
+                $interval = $start_date->diff($end_date);
+                if($interval->y >= 1){
+                    if($interval == 1) {
+                        $time_message = $interval->y . " year ago"; //1 year ago
+                    } else {
+                        $time_message = $interval->y . " years ago"; //more than 1 year ago
+                    }
+                } 
+                else if($interval->m >= 1) { //less than one year old, but at least a month old
+                    if ($interval->d == 0) { 
+                        $days = " ago"; //less than 1 day ago
+                    } 
+                    else if ($interval->d == 1) {
+                        $days = $interval->d . " day ago"; // 1 day ago
+                    } 
+                    else {
+                        $days = $interval->d . " days ago"; // more than 1 day ago
+                    }
+                    
+                    if($interval->m == 1) { //now check how many months old
+                        $time_message = $interval->m . " month" . $days; //1 month ago
+                    } 
+                    else {
+                        $time_message = $interval->m . " months" . $days; // more than 1 month ago
+                    }
+                } 
+                else if($interval->d >= 1) { //less than one month old, but at least one day old
+                    if ($interval->d == 1) {
+                        $time_message = "Yesterday"; // 1 day ago
+                    } 
+                    else {
+                        $time_message = $interval->d . " days ago"; // more than 1 day ago
+                    }
+                }
+                else if($interval->h >= 1) {
+                    if($interval->h == 1) {
+                        $time_message = $interval->h . " hour ago"; // 1 hour ago
+                    }
+                    else {
+                        $time_message = $interval->h . " hours ago"; // more than 1 hour ago
+                    }
+                }
+                else if($interval->i >= 1) {
+                    if($interval->i == 1) {
+                        $time_message = $interval->i . " minute ago"; // 1 minute ago
+                    }
+                    else {
+                        $time_message = $interval->i . " minutes ago"; // more than 1 minute ago
+                    }
+                }
+                else {
+                    if($interval->s < 30) {
+                        $time_message = "Just now"; // less than 30 seconds ago
+                    }
+                    else {
+                        $time_message = $interval->s . " seconds ago"; // 30 seconds or more seconds ago
+                    }
+                } //END if
+                
+                $user_obj = new User($con, $posted_by);
+            } //End While
+        }
     
+    ?>
     
+    <div class="comment_section">
+        <script>console.log("<?php echo $posted_by; ?>", "yes")</script>
+        <a href='<?php echo $posted_by; ?>' target="_parent">iron_man</a>
+    </div>
     
     
 

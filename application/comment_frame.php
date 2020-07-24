6eea76b2f678ffaf3ef601b4e97ed05fdcdb811c
Mark DeLuzio <deluziomark@gmail.com>
2020-07-24 07:20:52 -0400

    moved includes to the top of the file in comment_frame.php to solve cannot modify header issue

--- application/comment_frame.php
+++ application/comment_frame.php
@@ -1,3 +1,18 @@
+<?php
+    include '/home2/markdevnucitrus/connection.php'; 
+    include("includes/classes/User.php");
+    include("includes/classes/Post.php");
+    
+    
+    if(isset($_SESSION['username'])) {
+       $userLoggedIn =  $_SESSION['username'];
+       $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+       $user = mysqli_fetch_array($user_details_query);
+    } else {
+        header("Location: application/register.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,20 +31,7 @@
         }
     </style>
     
-    <?php
-        include '/home2/markdevnucitrus/connection.php'; 
-        include("includes/classes/User.php");
-        include("includes/classes/Post.php");
-        
-        
-        if(isset($_SESSION['username'])) {
-           $userLoggedIn =  $_SESSION['username'];
-           $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
-           $user = mysqli_fetch_array($user_details_query);
-        } else {
-            header("Location: application/register.php");
-        }
-    ?>
+    
     
     <script>
         function toggle() {

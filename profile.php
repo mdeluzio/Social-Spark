15bb522a72db32abd9b886d255911257860692ab
Mark DeLuzio <deluziomark@gmail.com>
2020-08-07 06:29:38 -0400

    set up base for profile.php

--- profile.php
+++ profile.php
@@ -1,6 +1,20 @@
-<?php  include("application/includes/header.php"); ?>
+<?php  
+    include("application/includes/header.php");
+    
+    if(isset($_GET['profile_username'])) {
+        $username = $_GET['profile_username'];
+        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username");
+        $user_array = mysqli_fetch_array($user_details_query);
+    }
+    
+?>
 
 
-Hello World
+    <div class="main_column column">
+        <?= $username; ?>
+        
+    </div>
     
-    
+    </div>
+</body>
+</html>

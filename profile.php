c9eff9c1cf6751a608577a25690f4c47e829b733
Mark DeLuzio <deluziomark@gmail.com>
2020-08-07 06:45:06 -0400

    added number of comments, likes, and friends to profile.php

--- profile.php
+++ profile.php
@@ -5,6 +5,8 @@
         $username = $_GET['profile_username'];
         $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
         $user_array = mysqli_fetch_array($user_details_query);
+        
+        $num_friends = (substr_count($user_array['friend_array'], ",")) -1;
     }
     
 ?>
@@ -12,7 +14,11 @@
     <div class="profile_left">
         <img src="<?= $user_array['profile_pic'] ?>">
         
-        
+        <div class="profile_info">
+            <p><?= "Posts: " . $user_array['num_posts']; ?></p>
+            <p><?= "Likes: " . $user_array['num_likes']; ?></p>
+            <p><?= "Friends: " . $num_friends; ?></p>
+        </div>
         
     </div>
 

72ed04c5d8a77f4b2bd6a0b2250e8ea05c5c88ab
Mark DeLuzio <deluziomark@gmail.com>
2020-08-07 06:39:36 -0400

    added user profile picture to profile.php

--- profile.php
+++ profile.php
@@ -3,16 +3,23 @@
     
     if(isset($_GET['profile_username'])) {
         $username = $_GET['profile_username'];
-        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username");
+        $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
         $user_array = mysqli_fetch_array($user_details_query);
     }
     
 ?>
 
+    <div class="profile_left">
+        <img src="<?= $user_array['profile_pic'] ?>">
+        
+        
+        
+    </div>
 
     <div class="main_column column">
         <?= $username; ?>
         
+       
     </div>
     
     </div>
